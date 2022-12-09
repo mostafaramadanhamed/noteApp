faff9b946408676538f2d43211fc9c48bc23a46e
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2022-12-09 06:34:25 +0200

    add all states

--- lib/cubit/notes_states.dart
+++ lib/cubit/notes_states.dart
@@ -1,2 +1,27 @@
 abstract class NotesStates{}
-class NotesInitialState extends NotesStates{}
+class InitialState extends NotesStates{}
+// Add Note
+class AddNoteLoadingState extends NotesStates{}
+class AddNoteSuccessState extends NotesStates{}
+class AddNoteIErrorState extends NotesStates{
+  final String error;
+
+  AddNoteIErrorState(this.error);
+}
+// Edit Note
+class EditNoteLoadingState extends NotesStates{}
+class EditNoteSuccessState extends NotesStates{}
+class EditNoteFailureState extends NotesStates{
+  final String error;
+
+  EditNoteFailureState(this.error);
+}
+// Delete Note
+class DeleteNoteLoadingState extends NotesStates{}
+class DeleteNoteSuccessState extends NotesStates{}
+class DeleteNoteFailureState extends NotesStates{
+  final String error;
+
+  DeleteNoteFailureState(this.error);
+}
+
