a12b41d10f8d6a0c7059572aff1c36a659a6a943
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-02-01 10:30:28 +0200

    create fetch notes method

--- lib/cubit/notes/notes_cubit.dart
+++ lib/cubit/notes/notes_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:whatsapp/cubit/notes/notes_states.dart';
+
+import '../../data/models/note_model.dart';
+import '../../view/constant.dart';
 
 class NotesCubit extends Cubit<NotesState>{
   NotesCubit():super(NoteInitialState());
 
+  fetchAllNotes() {
+    try{
+      var notesBox=Hive.box<NoteModel>(kBoxName);
+      List<NoteModel>notes=notesBox.values.toList();
+      emit(NoteSuccessState( notes ));
+    }
+    catch(ex){
+      emit(NoteFailureState(ex.toString()));
+    }
+  }
 
 }
--- lib/cubit/notes/notes_states.dart
+++ lib/cubit/notes/notes_states.dart
@@ -1,8 +1,15 @@
+import '../../data/models/note_model.dart';
+
 abstract class NotesState{}
 
 class NoteInitialState extends NotesState{}
 class NoteLoadingState extends NotesState{}
-class NoteSuccessState extends NotesState{}
+class NoteSuccessState extends NotesState{
+  final List<NoteModel>notes;
+
+  NoteSuccessState(this.notes);
+
+}
 class NoteFailureState extends NotesState{
   final String error;
 
